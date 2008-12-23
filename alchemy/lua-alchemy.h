e0c7fc93f998a64f7048f593b737c614ece2bc51
Robert Stehwien <rstehwien@gmail.com>
2008-12-22 17:20:19 -0700

    Extrace c<->Lua helper functions

--- alchemy/lua-alchemy.h
+++ alchemy/lua-alchemy.h
@@ -7,4 +7,17 @@ typedef unsigned char BOOL;
 #define TRUE (1)
 #define FALSE (0)
 
+/* Uncomment this to get a lot of debug traces */
+/*
+#define DO_SPAM 1
+*/
+
+#ifdef DO_SPAM
+  #define SPAM(a) sztrace a
+#else
+  #define SPAM(a) (void)0
+#endif /* DO_SPAM */
+
+void sztrace(char *);
+
 #endif /* LUA_ALCHEMY_H_ */
