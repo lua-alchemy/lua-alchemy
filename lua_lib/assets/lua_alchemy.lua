06b0500ec5b280f0f905c33818c3c053772aa3a0
Alexander Gladysh <agladysh@gmail.com>
2009-01-05 18:49:57 +0300

    as3.on_close()

--- lua_lib/assets/lua_alchemy.lua
+++ lua_lib/assets/lua_alchemy.lua
@@ -11,6 +11,7 @@ old_dofile("builtin://lua_alchemy/as3/canvas.lua")
 old_dofile("builtin://lua_alchemy/as3/sugar.lua")
 old_dofile("builtin://lua_alchemy/as3/file_get_contents.lua")
 old_dofile("builtin://lua_alchemy/as3/toobject.lua")
+old_dofile("builtin://lua_alchemy/as3/on_close.lua")
 
 old_dofile("builtin://lua_alchemy/lua/dofile.lua")
 old_dofile("builtin://lua_alchemy/lua/print.lua")
---
+++ lua_lib/assets/lua_alchemy/as3/on_close.lua
@@ -0,0 +1,34 @@
+-- Provides
+--   as3.on_close()
+
+do
+  local obj, list
+
+  local obj_mt =
+  {
+    __gc = function()
+      if list then
+        for i, fn in ipairs(list) do
+          local res, err = pcall(fn)
+          if not res then
+            as3.trace("error calling gc finalizer", i, fn)
+            as3.trace(err)
+          end
+        end
+      end
+    end;
+  }
+
+  as3.on_close = function(fn)
+    assert(type(fn) == "function", "function expected")
+
+    if not obj then
+      obj = newproxy()
+      debug.setmetatable(obj, obj_mt)
+
+      list = {}
+    end
+
+    list[#list + 1] = fn
+  end
+end
