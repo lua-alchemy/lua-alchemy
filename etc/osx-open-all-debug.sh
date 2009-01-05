bbca90781c6d276a70a3d63781fda7691dbf056a
Alexander Gladysh <agladysh@gmail.com>
2009-01-05 13:22:08 +0300

    etc/osx-open-all-debug.sh to open all debug .swfs in flash player for testing

---
+++ etc/osx-open-all-debug.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+open test/FluintLuaTests/build/debug/FluintLuaTests.swf
+open demo/LuaAlchemyDemo/build/debug/LuaAlchemyDemo.swf
+open demo/MiniDemo/build/debug/MiniDemo.swf
+open demo/Rapid/build/debug/Rapid.swf
