71a8402aaadabe1d7f1495852e6c92c739bb463a
Robert Stehwien <rstehwien@gmail.com>
2009-01-16 06:33:35 -0700

    Add comments and use sugar for pausecount demo

--- demo/LuaAlchemyDemo/assets/pausecount.lua
+++ demo/LuaAlchemyDemo/assets/pausecount.lua
@@ -1,24 +1,35 @@
-local vbox = as3.new("mx.containers::VBox")
-local label = as3.new("mx.controls::Label")
-local button = as3.new("mx.controls::Button")
+-- Count from 1 to a really big number, incrementing a label and yielding
+-- to Flash between each and allowing pause
+
+-- TODO flyield doesn't work as expected; flash times out and UI doesn't update
+
+-- Create controls
+local vbox = as3.class.mx.containers.VBox.new()
+local label = as3.class.mx.controls.Label.new()
+local button = as3.class.mx.controls.Button.new()
 
 local pause = false
 
-as3.set(button, "label", "Pause");
-as3.call(button, "addEventListener", "click",
+-- Setup pause button
+button.label = "Pause"
+pause_count = as3.toas3(
   function (e)
       pause = not pause
   end)
+button.addEventListener(as3.class.flash.events.MouseEvent.CLICK, pause_count)
 
-as3.call(vbox, "addChild", label)
-as3.call(vbox, "addChild", input)
-as3.call(vbox, "addChild", button)
-as3.call(canvas, "addChild", vbox)
+-- Add controls to canvas
+vbox.addChild(label)
+vbox.addChild(input)
+vbox.addChild(button)
+canvas.addChild(vbox)
 
+-- Funciton to get the timer count for use in delays
 function gettimer()
-  return as3.tolua(as3.namespacecall("flash.utils", "getTimer"))
+  return as3.tolua(as3.namespace.flash.utils.getTimer())
 end
 
+-- Function to delay X msec calling flyield between each msec
 function delay(msec)
   local start = gettimer()
   repeat
@@ -26,6 +37,7 @@ function delay(msec)
   until (gettimer() - start) >= msec
 end
 
+-- Cound from 1 to big number incrementing the counter every second
 local count = 0
 while true do
   delay(1000)
@@ -36,6 +48,13 @@ while true do
 
   if not pause then
     count = count + 1
-    as3.set(label, "text", count)
+    label.text = count
+    as3.trace("Count = " .. count)
   end
 end
+
+-- Remove the button event listener when Lua is closed
+as3.on_close(
+  function(e)
+    button.removeEventListener(as3.class.flash.events.MouseEvent.CLICK, pause_count)
+  end)
