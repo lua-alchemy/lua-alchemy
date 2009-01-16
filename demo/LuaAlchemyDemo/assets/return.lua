e43a49e7cbcfdfcb9bd5333b2b6356a2b526f479
Robert Stehwien <rstehwien@gmail.com>
2009-01-16 06:35:53 -0700

    Add comments and use sugar in return demo

--- demo/LuaAlchemyDemo/assets/return.lua
+++ demo/LuaAlchemyDemo/assets/return.lua
@@ -1,8 +1,18 @@
+-- Return a bunch of values from Lua, which come back as a Stack
+
 return
   true,
   "Hello from Lua",
   42,
   5.6+13,
   math.pi,
-  as3.new("String", "AS3 string"),
-  as3.new("Number", 128)
+  as3.class.String.new("AS3 string"),
+  as3.tolua(as3.class.Number.new(128)) + 2
+
+--[[
+Note that the String was not converted to a Lua type and the Number was
+Calling as3.tolua() was not necessary for the String since no Lua operation
+was performed on that value.  The Number needed conversion because of
+the addition operator (which wouldn't have worked if it was still an
+AS3 object)
+--]]
