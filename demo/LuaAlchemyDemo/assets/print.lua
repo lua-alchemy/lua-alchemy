9b4cae1ea711135642e97218cb83150e60c0e7df
Robert Stehwien <rstehwien@gmail.com>
2009-01-16 06:34:42 -0700

    Add comments and use sugar in print demo

--- demo/LuaAlchemyDemo/assets/print.lua
+++ demo/LuaAlchemyDemo/assets/print.lua
@@ -1,12 +1,15 @@
+-- Override print to go to canvas
+
 print = function(...)
  local n = select("#", ...)
  local t = {}
  for i = 1, n do
    t[i] = tostring(select(i, ...))
  end
- as3.set(output, "text", as3.get(output, "text") .. table.concat(t, "\t") .. "\n")
+ output.text = as3.tolua(output.text) .. table.concat(t, "\t") .. "\n"
 end
 
+-- Test print function
 print("Hello from Lua")
 print("Another print message")
 print("Yet another print message")
