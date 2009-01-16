f4ab46d268a3ae4bbfbc2323ef5720d56b89df57
Robert Stehwien <rstehwien@gmail.com>
2009-01-16 06:31:31 -0700

    Add comments and convert createasobject demo to use sugar

--- demo/LuaAlchemyDemo/assets/createasobject.lua
+++ demo/LuaAlchemyDemo/assets/createasobject.lua
@@ -1,19 +1,25 @@
+-- Create a dynamic AS3 object with some methods that increment values
+
+-- Override print so results can be displayed in canvas
 print = function(...)
  local t = {...}
  for k,v in pairs(t) do
    t[k] = tostring(v)
  end
- as3.set(output, "text", as3.get(output, "text") .. table.concat(t,"\t") .. "\n")
+ output.text = as3.tolua(output.text) .. table.concat(t,"\t") .. "\n"
 end
 
+-- Increment function
 local inc = function(self, n)
   self.i = self.i + (n or 1)
 end
 
+-- Get function
 local get = function(self)
  return self.i
 end
 
+-- One way to create a Lua "class" to increment and get
 local make_counter = function()
  return
  {
@@ -23,28 +29,33 @@ local make_counter = function()
  }
 end
 
+-- Create instances of Lua counter class
 local t1 = make_counter()
 local t2 = make_counter()
 
+-- Increment the Lua counter class
 t1:inc()
 t1:inc()
 t2:inc(5)
 
 print ("t1:", t1:get(), "t2:", t2:get())
 
+-- Function to create AS3 counter object that wraps Lua counter
 local counter_to_as = function(t)
-   local obj = as3.new("Object")
-   as3.set(obj, "inc", function(...) t:inc(...) end)
-   as3.set(obj, "get", function() return t:get() end)
+   local obj = as3.class.Object.new()
+   obj.inc = function(...) t:inc(...) end
+   obj.get = function() return t:get() end
    return obj
 end
 
+-- Create two AS3 counters
 local as_counter1 = counter_to_as(t1)
 local as_counter2 = counter_to_as(t2)
 
-as3.call(as_counter1, "inc")
-as3.call(as_counter2, "inc", 10)
+-- Incurement the AS3 counters
+as_counter1.inc()
+as_counter2.inc(10)
 
 print ("t1:", t1:get(), "t2:", t2:get())
 
-print ("as_counter1:", as3.call(as_counter1, "get"), "as_counter2:", as3.call(as_counter2, "get"))
+print ("as_counter1:", as3.tolua(as_counter1.get()), "as_counter2:", as3.tolua(as_counter2.get()))
