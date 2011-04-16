6bd157216eb8d344799409db71ebbdefd1b96429
Alexander Gladysh <agladysh@gmail.com>
2011-04-17 00:38:11 +0400

    etc: osx-open-all-release helper script

---
+++ etc/osx-open-all-release.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+open test/FluintLuaAlchemyTests/build/release/FluintLuaAlchemyTests.swf
+open demo/LuaAlchemyDemo/build/release/LuaAlchemyDemo.swf
+open demo/MiniDemo/build/release/MiniDemo.swf
+open demo/Rapid/build/release/Rapid.swf
