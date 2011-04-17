dcd37ac98a07340ddb41e708fc8bb3b3c298c5f0
Yura Beznos <yura.beznos@you-ra.info>
2011-04-18 03:13:01 +0400

    LuaBoy 0.001

--- etc/osx-open-all-debug.sh
+++ etc/osx-open-all-debug.sh
@@ -2,5 +2,6 @@
 
 open test/FluintLuaAlchemyTests/build/debug/FluintLuaAlchemyTests.swf
 open demo/LuaAlchemyDemo/build/debug/LuaAlchemyDemo.swf
+open demo/LuaBoy/build/debug/LuaBoy.swf
 open demo/MiniDemo/build/debug/MiniDemo.swf
 open demo/Rapid/build/debug/Rapid.swf
--- etc/osx-open-all-release.sh
+++ etc/osx-open-all-release.sh
@@ -2,5 +2,6 @@
 
 open test/FluintLuaAlchemyTests/build/release/FluintLuaAlchemyTests.swf
 open demo/LuaAlchemyDemo/build/release/LuaAlchemyDemo.swf
+open demo/LuaBoy/build/release/LuaBoy.swf
 open demo/MiniDemo/build/release/MiniDemo.swf
 open demo/Rapid/build/release/Rapid.swf
