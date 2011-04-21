e57695d87e5b94df6365902debfb9ba87ea978e7
Alexander Gladysh <agladysh@gmail.com>
2011-04-21 17:51:03 +0400

    demo: NoAssets

--- etc/osx-open-all-debug.sh
+++ etc/osx-open-all-debug.sh
@@ -2,5 +2,6 @@
 
 open test/FluintLuaAlchemyTests/build/debug/FluintLuaAlchemyTests.swf
 open demo/LuaAlchemyDemo/build/debug/LuaAlchemyDemo.swf
-open demo/MiniDemo/build/debug/MiniDemo.swf
 open demo/Rapid/build/debug/Rapid.swf
+open demo/MiniDemo/build/debug/MiniDemo.swf
+open demo/NoAssets/build/debug/NoAssets.swf
--- etc/osx-open-all-release.sh
+++ etc/osx-open-all-release.sh
@@ -2,5 +2,6 @@
 
 open test/FluintLuaAlchemyTests/build/release/FluintLuaAlchemyTests.swf
 open demo/LuaAlchemyDemo/build/release/LuaAlchemyDemo.swf
-open demo/MiniDemo/build/release/MiniDemo.swf
 open demo/Rapid/build/release/Rapid.swf
+open demo/MiniDemo/build/release/MiniDemo.swf
+open demo/NoAssets/build/release/NoAssets.swf
